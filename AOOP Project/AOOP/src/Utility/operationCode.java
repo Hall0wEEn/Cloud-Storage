614ebdfed292c16912fd53df63c54438f1c52df8
Sinderella <rawsec@gmail>
2013-11-16 21:25:33 +0700

    Operation code for clients and servers

---
+++ AOOP Project/AOOP/src/Utility/operationCode.java
@@ -0,0 +1,17 @@
+package Utility;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Touch
+ * Date: 11/16/13
+ * Time: 8:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class operationCode {
+	public static char HELO = 0xFF;
+	public static char REGISTER = 0x01;
+	public static char LOGIN = 0x02;
+	public static char LOGOUT = 0x03;
+	public static char UPLOAD = 0x04;
+	public static char DOWNLOAD = 0x05;
+}
