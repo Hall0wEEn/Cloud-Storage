b55b7248a32727ef5ca8122790f7b90ec9c5a952
Sinderella <rawsec@gmail>
2013-11-19 18:43:42 +0700

    Testing register op code

--- AOOP Project/AOOP/src/Client/cMain.java
+++ AOOP Project/AOOP/src/Client/cMain.java
@@ -1,9 +1,15 @@
 package Client;
 
+import Utility.operationCode;
+
 public class cMain {
 
 	public static void main(String[] args) {
-
+		try {
+			(new Thread(new Utility.send("127.0.0.1", 4444, operationCode.REGISTER, "touch|hello"))).start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
