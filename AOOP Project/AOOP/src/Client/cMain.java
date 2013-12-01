608fe6b34c4a6353e6bc3c370ad305d955cd7b1a
Ben Orenstein <ben.orenstein@gmail.com>
2013-12-01 12:43:49 +0700

    for testing

--- AOOP Project/AOOP/src/Client/cMain.java
+++ AOOP Project/AOOP/src/Client/cMain.java
@@ -2,11 +2,22 @@ package Client;
 
 import Utility.operationCode;
 
+import java.io.File;
+
 public class cMain {
 
 	public static void main(String[] args) {
 		try {
-			(new Thread(new Utility.send("127.0.0.1", 4444, operationCode.REGISTER, "touch|touch"))).start();
+			System.out.println("Registering....");
+			Thread t = (new Thread(new Utility.newSend("127.0.0.1", 4444, operationCode.REGISTER, "cloud|dbtest", "")));
+			t.start();
+			t.join();
+			System.out.println("Logging In");
+			t = (new Thread(new Utility.newSend("127.0.0.1", 4444, operationCode.LOGIN, "cloud|dbtest", "")));
+			t.start();
+			t.join();
+			System.out.println("Uploading file");
+			(new Thread(new Utility.newSend("127.0.0.1", 4444, operationCode.UPLOAD, new File("/Users/Touch/Desktop/test.png")))).start();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
