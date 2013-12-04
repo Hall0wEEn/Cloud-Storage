48783bad76225f1f188149922f3db881fc936626
Sinderella <rawsec@gmail>
2013-12-04 10:17:00 +0700

    Temp

--- AOOP Project/AOOP/src/Client/cMain.java
+++ AOOP Project/AOOP/src/Client/cMain.java
@@ -2,22 +2,22 @@ package Client;
 
 import Utility.operationCode;
 
-import java.io.File;
-
 public class cMain {
 
 	public static void main(String[] args) {
 		try {
+			System.out.println("Setting directory....");
+			new send("C:\\Users\\Touch\\Desktop\\cloud\\client");
 			System.out.println("Registering....");
-			Thread t = (new Thread(new Utility.newSend("127.0.0.1", 4444, operationCode.REGISTER, "cloud|dbtest", "")));
+			Thread t = (new Thread(new send("127.0.0.1", 4444, operationCode.REGISTER, "server|server", "")));
 			t.start();
 			t.join();
 			System.out.println("Logging In");
-			t = (new Thread(new Utility.newSend("127.0.0.1", 4444, operationCode.LOGIN, "cloud|dbtest", "")));
+			t = (new Thread(new send("127.0.0.1", 4444, operationCode.LOGIN, "server|server", "")));
 			t.start();
 			t.join();
 			System.out.println("Uploading file");
-			(new Thread(new Utility.newSend("127.0.0.1", 4444, operationCode.UPLOAD, new File("/Users/Touch/Desktop/test.png")))).start();
+			(new Thread(new send("127.0.0.1", 4444, operationCode.ALLHASH))).start();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
