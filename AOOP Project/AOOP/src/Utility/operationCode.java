fbeee521cb480c59a5643035821131953b872f64
Sinderella <rawsec@gmail>
2013-12-04 10:16:34 +0700

    Temp

--- AOOP Project/AOOP/src/Utility/operationCode.java
+++ AOOP Project/AOOP/src/Utility/operationCode.java
@@ -1,12 +1,5 @@
 package Utility;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Touch
- * Date: 11/16/13
- * Time: 8:48 PM
- * To change this template use File | Settings | File Templates.
- */
 public class operationCode {
 	public final static char HELO = 0xFF;
 	public final static char REGISTER = 0x01;
