eaa79341bda05c99bbd62a5ed007e9e15fdf736d
Poomkawin <poomkawin@outlook.com>
2013-12-14 12:27:29 +0700

    Client GUI
    Client GUI update 1

---
+++ AOOP Project/AOOP/src/Server/sGui.java
@@ -0,0 +1,44 @@
+package Server;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class sGui {
+
+	private JFrame frmServer;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					sGui window = new sGui();
+					window.frmServer.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public sGui() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmServer = new JFrame();
+		frmServer.setTitle("Server");
+		frmServer.setBounds(100, 100, 450, 300);
+		frmServer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
