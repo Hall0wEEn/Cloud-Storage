9f4b06e9c7bcef68133acf39cd4168b7972aca1d
Ben Orenstein <ben.orenstein@gmail.com>
2013-12-14 13:15:58 +0700

    using example

--- AOOP Project/AOOP/src/Client/cMain.java
+++ AOOP Project/AOOP/src/Client/cMain.java
@@ -7,17 +7,22 @@ public class cMain {
 	public static void main(String[] args) {
 		try {
 			System.out.println("Setting directory....");
-			new send("C:\\Users\\Touch\\Desktop\\cloud\\client");
+			new send("127.0.0.1", 4444, "/Users/Touch/Desktop/cloud/client/");
 			System.out.println("Registering....");
-			Thread t = (new Thread(new send("127.0.0.1", 4444, operationCode.REGISTER, "server|server", "")));
+			Thread t = (new Thread(new send(operationCode.REGISTER, "server|server", "")));
 			t.start();
 			t.join();
 			System.out.println("Logging In");
-			t = (new Thread(new send("127.0.0.1", 4444, operationCode.LOGIN, "server|server", "")));
+			t = (new Thread(new send(operationCode.LOGIN, "server|server", "")));
 			t.start();
 			t.join();
 			System.out.println("Uploading file");
-			(new Thread(new send("127.0.0.1", 4444, operationCode.ALLHASH))).start();
+			(new Thread(new send(operationCode.HELO, "", ""))).start();
+//			(new Thread(new send(operationCode.SPACE, "", ""))).start();
+//			(new Thread(new send(operationCode.DELETE, "c.zip", ""))).start();
+//			(new Thread(new send(operationCode.DOWNLOAD, "test.txt", ""))).start();
+//			(new Thread(new send(operationCode.UPLOAD, new File("/Users/Touch/Desktop/cloud/client/test.pdf")))).start();
+//			(new Thread(new send(operationCode.ALLHASH))).start();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
