92b136ebb1f18708c280bc326455c6d1f2023921
Poomkawin <poomkawin@outlook.com>
2013-12-14 18:36:48 +0700

    Test Class

---
+++ AOOP Project/AOOP/src/Server/test.java
@@ -0,0 +1,34 @@
+package Server;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.management.*;
+
+/**
+ * Created by Mon on 12/14/13.
+ */
+public class test {
+	public static void main(String[] args) throws IOException {
+		final OperatingSystemMXBean myOsBean = ManagementFactory.getOperatingSystemMXBean();
+//		while (true) {
+//			double load = myOsBean.getSystemLoadAverage();
+//			System.out.println(load / 8 * 100);
+//		}
+		System.out.println(Runtime.getRuntime().availableProcessors());
+
+//		File[] roots = File.listRoots();
+//
+//		for (File root : roots) {
+//			System.out.println("File system root: " + root.getAbsolutePath());
+//			System.out.println("Total space (bytes): " + root.getTotalSpace() / 999962804.306);
+//			System.out.println("Free space (bytes): " + root.getFreeSpace() / 999962804.306);
+//			System.out.println("Usable space (bytes): " + root.getUsableSpace() / 999962804.306);
+//		}
+
+
+		System.out.println(Runtime.getRuntime().totalMemory());
+	}
+
+	private void test () {
+	}
+}
