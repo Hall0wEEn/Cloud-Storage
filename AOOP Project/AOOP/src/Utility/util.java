08e5a5458372ba3aa300e3cef4526c05ba19704e
Sinderella <rawsec@gmail>
2013-12-14 13:17:40 +0700

    add some method

--- AOOP Project/AOOP/src/Utility/util.java
+++ AOOP Project/AOOP/src/Utility/util.java
@@ -1,7 +1,30 @@
 package Utility;
 
+import java.io.File;
+
 public class util {
+	public static long folderSize(File directory) {
+		long length = 0;
+		for (File file : directory.listFiles()) {
+			if (file.isFile())
+				length += file.length();
+			else
+				length += folderSize(file);
+		}
+		return length;
+	}
+
 	public static boolean isWin() {
 		return System.getProperty("os.name").toLowerCase().contains("win");
+	}
+
+	public static String getExt(File file) {
+		String extension = "";
+
+		int i = file.toString().lastIndexOf('.');
+		if (i > 0) {
+			extension = file.toString().substring(i + 1);
+		}
+		return extension;
 	}
 }
