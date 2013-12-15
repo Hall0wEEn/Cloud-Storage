a779d2051d2d03fee2b4ee4485008ed970d23726
Ben Orenstein <ben.orenstein@gmail.com>
2013-12-15 12:55:16 +0700

    example

--- AOOP Project/AOOP/src/Client/cMain.java
+++ AOOP Project/AOOP/src/Client/cMain.java
@@ -2,26 +2,28 @@ package Client;
 
 import Utility.operationCode;
 
+import java.io.File;
+
 public class cMain {
 
 	public static void main(String[] args) {
 		try {
 			System.out.println("Setting directory....");
-			new send("127.0.0.1", 4444, "/Users/Touch/Desktop/cloud/client/");
+			new send("127.0.0.1", 4444, "/Users/Touch/Cloud Storage/test/");
 			System.out.println("Registering....");
-			Thread t = (new Thread(new send(operationCode.REGISTER, "server|server", "")));
+			Thread t = (new Thread(new send(operationCode.REGISTER, "test|test", "")));
 			t.start();
 			t.join();
 			System.out.println("Logging In");
-			t = (new Thread(new send(operationCode.LOGIN, "server|server", "")));
+			t = (new Thread(new send(operationCode.LOGIN, "test|test", "")));
 			t.start();
 			t.join();
 			System.out.println("Uploading file");
-			(new Thread(new send(operationCode.HELO, "", ""))).start();
+//			(new Thread(new send(operationCode.HELO, "", ""))).start();
 //			(new Thread(new send(operationCode.SPACE, "", ""))).start();
 //			(new Thread(new send(operationCode.DELETE, "c.zip", ""))).start();
 //			(new Thread(new send(operationCode.DOWNLOAD, "test.txt", ""))).start();
-//			(new Thread(new send(operationCode.UPLOAD, new File("/Users/Touch/Desktop/cloud/client/test.pdf")))).start();
+			(new Thread(new send(operationCode.UPLOAD, new File("/Users/Touch/Cloud Storage/test/intellij-13-keygen/keygen.class")))).start();
 //			(new Thread(new send(operationCode.ALLHASH))).start();
 		} catch (Exception e) {
 			e.printStackTrace();
